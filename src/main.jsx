2d017f2722add47c68cd98ae2fe4b9a0c0f28766
sachinjayasinghe <155840534+sachinjayasinghe@users.noreply.github.com>
2025-03-08 19:03:41 +0530

    create category crud

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
+import ResCategories from "./pages/Departments/Restaurant/Categories/ResCategories.jsx";
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    hello hi
-  </StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/*" element={<App />} />
+        <Route path="/restaurant/categories" element={<ResCategories />} />
+      </Routes>
+    </Router>
+  </StrictMode>
+);
