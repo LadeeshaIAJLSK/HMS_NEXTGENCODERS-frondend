cc96d9a17f131919a8354d334c0480fdd5e9dd07
sachinjayasinghe <155840534+sachinjayasinghe@users.noreply.github.com>
2025-03-11 19:13:43 +0530

    created products page

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 import ResCategories from "./pages/Departments/Restaurant/Categories/ResCategories.jsx";
-
+import Products from "./pages/Departments/Restaurant/Products/Products";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -12,6 +12,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/*" element={<App />} />
         <Route path="/restaurant/categories" element={<ResCategories />} />
+        <Route path="/restaurant/products" element={<Products />} />
       </Routes>
     </Router>
   </StrictMode>
