ce3100ce9c9a2c9f66b9b6c6c5aa8e21e3d54a9e
LadeeshaIAJLSK <jazzladeesha@gmail.com>
2025-03-13 14:46:22 +0530

    first commit

---
+++ src/pages/Departments/Reception/DOReservations/info1/Info1.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CheckInForm from "../../../../.././components/reception/CheckInForm/CheckInForm"; // Adjust path if needed
+
+const Info1 = () => {
+  return (
+    <div>
+      
+      <CheckInForm />
+    </div>
+  );
+};
+
+export default Info1;
+
