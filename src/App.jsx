91a4d00c757ac2c2a3774415e402bb5a9799ce3a
LadeeshaIAJLSK <jazzladeesha@gmail.com>
2025-03-15 19:24:54 +0530

    Updated page2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import Info1 from "./pages/Departments/Reception/DOReservations/info1/Info1"; // Adjust path if needed
+import Info1 from "./pages/Departments/Reception/DOReservations/info1/Info1";
+import Info2 from "./pages/Departments/Reception/DOReservations/info2/info2"; 
+// Adjust path if needed
 
 const App = () => {
   return (
     <div>
       <Info1 />
+      <Info2 />
     </div>
   );
 };
---
+++ src/pages/Departments/Reception/DOReservations/info2/Info2.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Reservation1 from "../../../../../components/reception/Reservation/reservation1";
+import Reservation2 from "../../../../../components/reception/Reservation/reservation2"; // Adjust path if needed
+
+const Info2 = () => {
+  return (
+    <div>
+        <Reservation1 />
+        <Reservation2 />
+      
+     
+
+    </div>
+  );
+};
+
+export default Info2;
