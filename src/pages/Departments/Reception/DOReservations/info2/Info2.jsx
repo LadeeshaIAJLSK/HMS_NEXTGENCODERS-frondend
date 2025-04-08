69a582a486dcd6d79100f3a3b03cfd5f4da95ff8
LadeeshaIAJLSK <jazzladeesha@gmail.com>
2025-04-09 00:12:52 +0530

    Your commit message

--- src/pages/Departments/Reception/DOReservations/info2/Info2.jsx
+++ src/pages/Departments/Reception/DOReservations/info2/Info2.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import Reservation1 from "../../../../../components/reception/Reservation/reservation1";
-import Reservation2 from "../../../../../components/reception/Reservation/reservation2"; // Adjust path if needed
+import Reservation2 from "../../../../../components/reception/Reservation/reservation2";
+import Sidebar from "../../../../../components/reception/recSidebar/Recsidebar";
 
 const Info2 = () => {
   return (
-    <div>
+    <div style={{ 
+      display: 'grid',
+      gridTemplateColumns: '20% 80%',
+     
+     
+      overflowX: 'hidden'
+    }}>
+      <div style={{ overflowY: 'auto' }}>
+        <Sidebar />
+      </div>
+    
+      <div style={{ 
+        padding: '20px',
+        overflowY: 'auto',
+        boxSizing: 'border-box'
+      }}>
         <Reservation1 />
         <Reservation2 />
-      
-     
-
+      </div>
     </div>
   );
 };
