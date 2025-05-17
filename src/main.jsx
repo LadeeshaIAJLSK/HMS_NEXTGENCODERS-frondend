c5ba3e2a375a41c40d88849b47bbb38a663e6583
Abdulla <abdullahishak3034@gmail.com>
2025-05-17 23:09:32 +0530

    Add files to my branch

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import './index.css';
-import App from './App.jsx';
-import ResCategories from "./pages/Departments/Restaurant/Categories/ResCategories.jsx";
-import Products from "./pages/Departments/Restaurant/Products/Products";
-import RoomHome from "./pages/Departments/Owner/OwnRooms/RoomHome.jsx";
-import CreateRooms from "./pages/Departments/Owner/OwnRooms/CreateRooms";
-import EditRooms from "./pages/Departments/Owner/OwnRooms/EditRooms";
-import RoomDetails from "./pages/Departments/Owner/OwnRooms/RoomDetails";
-import Sidebar from "./components/owner/ownSidebar/Ownsidebar.jsx"; 
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/restaurant/categories" element={<ResCategories />} />
-        <Route path="/restaurant/products" element={<Products />} />
-
-        {/* Corrected Room Management Routes */}
-        <Route path="/rooms/home" element={<RoomHome />} />
-        <Route path="/rooms/add" element={<CreateRooms />} />
-        <Route path="/rooms/edit/:id" element={<EditRooms />} />
-        <Route path="/rooms/:id" element={<RoomDetails />} />
-
-        {/* Redirect unknown routes to "/rooms/home" */}
-        <Route path="*" element={<Navigate replace to="/rooms/home" />} />
-      </Routes>
-    </Router>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
